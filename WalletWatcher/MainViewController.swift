f793bf8b8f725fe8e5ea130c8a3fc29f239dd30e
Mario <mario.cordova.862@my.csun.edu>
2017-06-18 12:19:46 -0700

    Created the main view with a table view and two buttons.

--- WalletWatcher/MainViewController.swift
+++ WalletWatcher/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  WalletWatcher
 //
 //  Created by Mario Cordova on 6/18/17.
@@ -8,17 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
