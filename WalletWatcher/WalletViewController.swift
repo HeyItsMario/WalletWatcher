760c0b45a1ec0cad2b5ef2b69be5a6782c49e5c6
Mario <mario.cordova.862@my.csun.edu>
2017-06-21 10:54:42 -0700

    Got the wallet view to display the name of the store

--- WalletWatcher/WalletViewController.swift
+++ WalletWatcher/WalletViewController.swift
@@ -26,14 +26,13 @@ class WalletViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print((budget?.expense?.count)!)
         return (budget?.expense?.count)!
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        cell.textLabel?.text = "Wallet"
-        let myArray = Array(budget?.value(forKey: "expense"))
+        let myArray = Array((budget?.expense)!)
+        cell.textLabel?.text = (myArray[indexPath.row] as! Expense).store
         return cell
     }
     
